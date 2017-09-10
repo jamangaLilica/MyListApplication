e424dd2549fdccf8ea165f6302ab28e7ae856bc7
Almeida Jessica <almeida.jessica@outlook.com>
2017-09-10 17:19:58 +0200

    upload base project

---
+++ app/src/live/java/com/jamanga/mylist/live/LiveAccountServices.java
@@ -0,0 +1,8 @@
+package com.jamanga.mylist.live;
+
+/**
+ * Created by almei on 10.09.2017.
+ */
+
+public class LiveAccountServices {
+}
---
+++ app/src/main/java/com/jamanga/mylist/infrastructure/Utils.java
@@ -0,0 +1,8 @@
+package com.jamanga.mylist.infrastructure;
+
+/**
+ * Created by almei on 10.09.2017.
+ */
+
+public class Utiles {
+}
